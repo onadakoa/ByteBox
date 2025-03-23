7fd8a03d23f2aa4c9c1dd67f6f1109aa55d0e456
onadakoa <onadakoa@gmail.com>
2025-03-23 21:17:26 +0100

    Loading (will change), /auth backend connection

--- components/Form/AuthForm.tsx
+++ components/Form/AuthForm.tsx
@@ -2,57 +2,102 @@
 import css from "./AuthForm.module.css"
 import {poppins, roboto} from "@/utils/font";
 import {Input} from "@/components/Form/Input";
-import {FormEventHandler, useState} from "react";
+import {FormEventHandler, Fragment, useEffect, useState} from "react";
+import {API_HOSTNAME} from "@/utils/api";
+import {Packet} from "@/utils/Packet";
+import {Loading} from "@/components/Loading/Loading";
+import {useRouter} from "next/navigation";
 
 
 export function AuthForm() {
     const [isLogin, setIsLogin] = useState<boolean>(true);
+    const [isLoading, setIsLoading] = useState(true);
+    const router = useRouter();
 
     const Submit: FormEventHandler<HTMLFormElement> = (event) => {
         event.preventDefault();
+        setIsLoading(true);
         const data = new FormData(event.target as HTMLFormElement);
-        console.log(data);
 
-        fetch("http://localhost:8080").then(body => body.json().then(json => {
-            console.log(json);
-        }))
+        let href = API_HOSTNAME + ((isLogin) ? "login.php" : "register.php");
+
+        fetch(href, {
+            credentials: "include",
+            method: "POST",
+            body: data,
+        }).then(body => {
+            body.json().then((json: Packet) => {
+                    if (json.c) {
+                        console.log("error");
+                        console.log(json);
+                        return;
+                    }
+                    console.log(json);
+                    setIsLoading(false);
+                }
+            )
+        })
     }
+
+    useEffect(() => {
+        fetch(API_HOSTNAME, {
+            credentials: "include",
+        }).then(body => body.json().then((json: Packet) => {
+            if (json.d.user != 0) {
+                router.push("/");
+                return;
+            }
+            setIsLoading(false);
+        }))
+    }, []);
 
     return (
         <div className={css.container}>
             <div className={css.formBody}>
-                <div style={poppins.style} className={css.formTitle}>
-                    {(isLogin) ? "Zaloguj" : "Zarejestruj"}
-                </div>
-                <form onSubmit={Submit} style={roboto.style} className={css.formContent}>
-                    {(!isLogin) && (
-                        <>
-                            <div>Full name</div>
-                            <div>
-                                <Input name={"fullName"} placeholder={"John Smith"}/>
-                            </div>
-                        </>
-                    )}
-                    <div>E-mail</div>
-                    <div>
-                        <Input name={"email"} type={"email"} placeholder={"John@example.org"}/>
+                {(isLoading) ? (<>
+                    <Loading/>
+                </>) : (<>
+                    <div style={poppins.style} className={css.formTitle}>
+                        {(isLogin) ? "Zaloguj" : "Zarejestruj"}
                     </div>
-                    <div>Password</div>
-                    <div>
-                        <Input name={"password"} type={"password"} placeholder={"Your password"}/>
+                    <form onSubmit={Submit} style={roboto.style} className={css.formContent}>
+                        {(isLogin) ? (<LoginFormProps/>) : (<RegisterFormProps/>)}
+
+                        <div className={css.submit}>
+                            <input type="submit" value={(isLogin) ? "Zaloguj" : "Zarejestruj"}/>
+                        </div>
+                    </form>
+                    <div style={poppins.style} className={css.modeChanger}>
+                        {
+                            (isLogin) ? "Nie masz konta?" : "Masz już konto?"
+                        } <span onClick={() => setIsLogin(!isLogin)}>{
+                        (isLogin) ? "Zarejestruj" : "Zaloguj"
+                    } się!</span>
                     </div>
-                    <div className={css.submit}>
-                        <input type="submit" value={(isLogin) ? "Zaloguj" : "Zarejestruj"}/>
-                    </div>
-                </form>
-                <div style={poppins.style} className={css.modeChanger}>
-                    {
-                        (isLogin) ? "Nie masz konta?" : "Masz już konto?"
-                    } <span onClick={() => setIsLogin(!isLogin)}>{
-                    (isLogin) ? "Zarejestruj" : "Zaloguj"
-                } się!</span>
-                </div>
+                </>)}
+
             </div>
         </div>
     );
+}
+
+function LoginFormProps() {
+    return (
+        <>
+            <div>Login</div>
+            <div><Input name={"login"} type={"text"} placeholder={"John@example.com"}></Input></div>
+            <div>Password</div>
+            <div><Input name={'password'} type={"password"} placeholder={"Your pasword"}></Input></div>
+        </>
+    );
+}
+
+function RegisterFormProps() {
+    return (<>
+        <LoginFormProps/>
+        <div>First Name</div>
+        <div><Input name={"first_name"} type={"text"} placeholder={"John"}/></div>
+        <div>Last Name</div>
+        <div><Input name={"last_name"} type={"text"} placeholder={"Smith"}/></div>
+    </>);
 }
