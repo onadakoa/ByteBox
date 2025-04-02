975cdf6054108ee916ea46210f1493ac14d17407
onadakoa <onadakoa@gmail.com>
2025-04-02 11:31:47 +0200

    Logo fixed svg path

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -2,5 +2,5 @@ import Image from "next/image";
 
 
 export default function Logo({sizes}: { sizes?: string }) {
-    return (<Image sizes={sizes} src={'logo.svg'} alt="ByteBox Logo" fill/>);
+    return (<Image sizes={sizes} src={'/logo.svg'} alt="ByteBox Logo" fill/>);
 }
