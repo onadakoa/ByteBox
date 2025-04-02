da1ef844d99493184cf264025285ca1cd77e267d
onadakoa <onadakoa@gmail.com>
2025-04-02 11:21:59 +0200

    font's fallback

--- utils/font.ts
+++ utils/font.ts
@@ -3,9 +3,11 @@ import {Poppins, Roboto} from "next/font/google";
 
 export const poppins = Poppins({
     weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
-    subsets: ['latin']
+    subsets: ['latin'],
+    fallback: ["sans-serif"],
 })
 export const roboto = Roboto({
     weight: ["100", "300", "400", "500", "700", "900"],
-    subsets: ['latin']
+    subsets: ['latin'],
+    fallback: ["sans-serif"],
 })
