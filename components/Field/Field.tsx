846a3391612ab2457509ade7d763f4bd35891fc0
onadakoa <onadakoa@gmail.com>
2025-04-04 12:16:23 +0200

    Field component width, height values

--- components/Field/Field.tsx
+++ components/Field/Field.tsx
@@ -1,9 +1,11 @@
 import css from "./Field.module.css"
 import {CSSProperties} from "react";
 
-export function Field(ctx: { children?: React.ReactNode, padding?: string }) {
+export function Field(ctx: { children?: React.ReactNode, padding?: string, height?: string, width?: string }) {
     const style: CSSProperties = {
-        padding: ctx.padding
+        padding: ctx.padding,
+        height: ctx.height,
+        width: ctx.width,
     }
     return (
         <div className={css.container} style={style}>
