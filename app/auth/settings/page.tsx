ebf462d66718cf0be97816aa1901f7ae3e08d54b
onadakoa <onadakoa@gmail.com>
2025-04-05 13:27:55 +0200

    Settings route

---
+++ app/auth/settings/page.tsx
@@ -0,0 +1,21 @@
+import css from "./page.module.css"
+import {poppins} from "@/utils/font";
+import {Header} from "@/components/Header/Header";
+import {Field} from "@/components/Field/Field";
+import {UserSettings} from "@/components/Settings/UserSettings";
+import {BillingAddressManager} from "@/components/Settings/BillingAddressManager";
+
+export default function Page() {
+    return (<div className={css.mainContainer}>
+        <div className={css.container} style={poppins.style}>
+            <Header>Settings</Header>
+            <Field padding={"15px 20px"}>
+                <UserSettings/>
+            </Field>
+            <Header>Billing address</Header>
+            <Field padding={"15px 20px"}>
+                <BillingAddressManager/>
+            </Field>
+        </div>
+    </div>)
+}
