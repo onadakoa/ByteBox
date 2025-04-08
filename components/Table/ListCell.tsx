3a5535b1bb98bced1e6ac66d05ef20ed88f950f6
onadakoa <onadakoa@gmail.com>
2025-04-08 10:29:17 +0200

    ListCell `centerHorizontally` prop

--- components/Table/ListCell.tsx
+++ components/Table/ListCell.tsx
@@ -1,9 +1,10 @@
 import css from "./ListCell.module.css"
 import React, {CSSProperties} from "react";
 
-export const ListCell = (props: { children?: React.ReactNode, className?: string, padding?: string, }) => {
+export const ListCell = (props: { children?: React.ReactNode, className?: string, padding?: string, centerHorizontal?: boolean }) => {
     const style: CSSProperties = {
         padding: props.padding,
+        justifyContent: (props.centerHorizontal) ? "center" : undefined,
     }
     return (
         <div className={[css.container, props.className].join(" ")} style={style}>
