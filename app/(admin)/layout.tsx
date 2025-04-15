28d46baf14ae908063042da95cfdfdb1e966ecb9
onadakoa <onadakoa@gmail.com>
2025-04-15 11:32:48 +0200

    /dashboard layout, and page files setup

---
+++ app/(admin)/layout.tsx
@@ -0,0 +1,14 @@
+import React, {ReactNode} from "react";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "Admin Dashboard",
+}
+
+export default function layout(props: {children: ReactNode}) {
+
+    return (
+        <>
+        </>
+    );
+}
