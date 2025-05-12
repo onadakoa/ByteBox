fce646c74505746b0f1f80af7e107eb03e18c264
onadakoa <onadakoa@gmail.com>
2025-05-12 14:38:04 +0200

    +`ApiFeed` fetches product's list

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,9 +1,8 @@
-import Symbol from "@/components/MaterialSymbols/Symbol"
 import css from "./page.module.css"
 import FilterBox from "@/components/Filter/FilterBox";
 import {Suspense} from "react";
-import Feed from "@/components/Feed/Feed";
 import FeedController from "@/components/Feed/FeedController";
+import {ApiFeed} from "@/components/Api/ApiFeed";
 
 export default function Page() {
 
@@ -12,24 +11,7 @@ export default function Page() {
             <Suspense>
                 <FilterBox/>
                 <FeedController>
-                    <Feed price={233} title={"AKKO keyboard 5325B plus v2"} type={"active"} productHref={"/product"}/>
-                    <Feed price={233} title={"AKKO keyboard 5325B plus v2"} type={"active"} productHref={"/product"}/>
-                    <Feed price={233} title={"AKKO keyboard 5325B plus v2"} type={"active"} productHref={"/product"}/>
-                    <Feed price={233} title={"AKKO keyboard 5325B plus v2"} type={"active"} productHref={"/product"}/>
-                    <Feed price={233} title={"AKKO keyboard 5325B plus v2"} type={"active"} productHref={"/product"}/>
-                    <Feed price={233} title={"AKKO keyboard 5325B plus v2"} type={"active"} productHref={"/product"}/>
-                    <Feed price={233} title={"AKKO keyboard 5325B plus v2"} type={"active"} productHref={"/product"}/>
-                    <Feed price={233} title={"AKKO keyboard 5325B plus v2"} type={"active"} productHref={"/product"}/>
-                    <Feed price={233} title={"AKKO keyboard 5325B plus v2"} type={"active"} productHref={"/product"}/>
-                    <Feed price={233} title={"AKKO keyboard 5325B plus v2"} type={"active"} productHref={"/product"}/>
-                    <Feed price={233} title={"AKKO keyboard 5325B plus v2"} type={"active"} productHref={"/product"}/>
-                    <Feed price={233} title={"AKKO keyboard 5325B plus v2"} type={"active"} productHref={"/product"}/>
-                    <Feed price={233} title={"AKKO keyboard 5325B plus v2"} type={"active"} productHref={"/product"}/>
-                    <Feed price={233} title={"AKKO keyboard 5325B plus v2"} type={"active"} productHref={"/product"}/>
-                    <Feed price={233} title={"AKKO keyboard 5325B plus v2"} type={"active"} productHref={"/product"}/>
-                    <Feed price={233} title={"AKKO keyboard 5325B plus v2"} type={"active"} productHref={"/product"}/>
-                    <Feed price={233} title={"AKKO keyboard 5325B plus v2"} type={"active"} productHref={"/product"}/>
-                    <Feed price={233} title={"AKKO keyboard 5325B plus v2"} type={"active"} productHref={"/product"}/>
+                    <ApiFeed/>
                 </FeedController>
             </Suspense>
         </div>
---
+++ components/Api/ApiFeed.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import useProductList from "@/hooks/useProductList";
+import {useEffect, useState} from "react";
+import Feed from "@/components/Feed/Feed";
+import {Product} from "@/utils/Product";
+import {Attachment} from "@/utils/Attachment";
+
+export const ApiFeed = () => {
+    const {products, isProductsLoading, error} = useProductList();
+
+    if (isProductsLoading || error) return "loading";
+
+    return (
+        <>
+            {products?.map((prod) => {
+                let ApiProduct = Product.getFilled(prod);
+                return <ApiFeedContent product={ApiProduct} key={ApiProduct.product_id}/>
+            })}
+        </>
+    );
+};
+
+export const ApiFeedContent = (props: {
+    product: Product,
+}) => {
+    const [attachment, setAttachment] = useState<Attachment | null>(null);
+
+    useEffect(() => {
+        props.product.fetchAttachment()?.then(att => {
+            setAttachment(att)
+        })
+    }, []);
+
+    return (<Feed
+        price={props.product.price} title={props.product.name} type={(props.product.stock > 0) ? "active" : "unavailable"}
+        imageSrc={(attachment) ? attachment.getImages()[0].getURL() : undefined}
+        productHref={"/product?id=" + props.product.product_id}
+    />)
+}
