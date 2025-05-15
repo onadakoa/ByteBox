c543fa57b87bbec1f0b5bbfac7fa58f6a34e5033
onadakoa <onadakoa@gmail.com>
2025-05-15 18:10:39 +0200

    /auth now check user login status (you have to be logged out now)

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,10 +1,12 @@
 import css from "./page.module.css"
 import {AuthForm} from "@/components/Form/AuthForm";
+import {RequireAuth} from "@/components/Api/RequireAuth";
 
 export default function Page() {
 
     return (
         <div className={css.container}>
+            <RequireAuth type={"loggedOut"}/>
             <AuthForm/>
         </div>
     );
