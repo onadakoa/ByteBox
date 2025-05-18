77baa9745997eb65a4563fcc8e908c48084ddcfc
onadakoa <onadakoa@gmail.com>
2025-05-18 18:49:34 +0200

    ImageSelector fetches images from given `attachment_id`

--- components/ProductView/ImageSelector.tsx
+++ components/ProductView/ImageSelector.tsx
@@ -1,7 +1,8 @@
 "use client"
 import css from "./ImageSelector.module.css"
 import Image from "next/image";
-import {CSSProperties, useState} from "react";
+import {CSSProperties, useEffect, useState} from "react";
+import {Attachment} from "@/utils/Attachment";
 
 function Item(props: { href: string, active?: boolean, onClick?: () => void }) {
     const style: CSSProperties = {
@@ -12,14 +13,25 @@ function Item(props: { href: string, active?: boolean, onClick?: () => void }) {
     </div>);
 }
 
-export function ImageSelector() {
-    const images = [
+export function ImageSelector(props: { attachment_id?: number }) {
+    const [images, setImages] = useState([
         "https://placehold.co/600x600",
         "https://placehold.co/400x600",
         "https://placehold.co/600x400",
         "https://placehold.co/300x400",
-    ]
+    ])
     const [pointer, setPointer] = useState(0);
+
+    useEffect(() => {
+        if (!props.attachment_id) return;
+        Attachment.fetchById(props.attachment_id).then(att => {
+            let nImages: string[] = [];
+            att.getImages().forEach(img => {
+                nImages.push(img.getURL())
+            })
+            setImages(nImages);
+        })
+    }, [props.attachment_id]);
 
     return (
         <div className={css.container}>
