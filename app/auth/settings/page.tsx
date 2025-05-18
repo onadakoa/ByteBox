02bb099df7d4db77b8661f0120743a6267b8c399
onadakoa <onadakoa@gmail.com>
2025-05-18 11:56:44 +0200

    /auth/settings route checks if a user is logged

--- app/auth/settings/page.tsx
+++ app/auth/settings/page.tsx
@@ -5,9 +5,11 @@ import {Field} from "@/components/Field/Field";
 import {UserSettings} from "@/components/Settings/UserSettings";
 import {BillingAddressManager} from "@/components/Settings/BillingAddressManager";
 import {LoginManager} from "@/components/Settings/LoginManager";
+import {RequireAuth} from "@/components/Api/RequireAuth";
 
 export default function Page() {
     return (<div className={css.mainContainer}>
+        <RequireAuth type={"loggedIn"}/>
         <div className={css.container} style={poppins.style}>
             <Header>Settings</Header>
             <Field padding={"15px 20px"}>
