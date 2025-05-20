48e5d3549f9c6ea1a993dc04aad9a503f39753c1
onadakoa <onadakoa@gmail.com>
2025-05-20 22:39:05 +0200

    NavBar dynamic icons (depends on login status)

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -4,6 +4,9 @@ import Symbol from "../MaterialSymbols/Symbol"
 import Logo from "../Logo/Logo"
 import SearchBar from "../SearchBar/SearchBar"
 import {Suspense} from "react"
+import {IfIsLoggedIn} from "@/components/Api/IfIsLoggedIn";
+import {IfIsAdmin} from "@/components/Api/IfIsAdmin";
+import {Test} from "@/components/Api/Test";
 
 
 export default function NavBar() {
@@ -20,21 +23,28 @@ export default function NavBar() {
             </Suspense>
         </div>
         <div className={css.iconsContainer}>
-            <Link href={"/dashboard"} title={"Dashboard"}>
-                <Symbol>dashboard</Symbol>
-            </Link>
-            <Link href={"/auth/orders"} title={"zamowienia"}>
-                <Symbol>receipt_long</Symbol>
-            </Link>
-            <Link href={"/auth"} title={"zaloguj"}>
-                <Symbol>account_circle</Symbol>
-            </Link>
-            <Link href={"/cart"} title={"koszyk"}>
-                <Symbol>shopping_cart</Symbol>
-            </Link>
-            <Link href={"/auth/settings"} title={"ustawienia"}>
-                <Symbol>settings_account_box</Symbol>
-            </Link>
+            <IfIsAdmin>
+                <Link href={"/dashboard"} title={"Dashboard"}>
+                    <Symbol>dashboard</Symbol>
+                </Link>
+            </IfIsAdmin>
+            <IfIsLoggedIn
+                ifNotLogged={
+                    <Link href={"/auth"} title={"Login"}>
+                        <Symbol>account_circle</Symbol>
+                    </Link>
+                }
+            >
+                <Link href={"/auth/orders"} title={"Orders"}>
+                    <Symbol>receipt_long</Symbol>
+                </Link>
+                <Link href={"/cart"} title={"koszyk"}>
+                    <Symbol>shopping_cart</Symbol>
+                </Link>
+                <Link href={"/auth/settings"} title={"ustawienia"}>
+                    <Symbol>settings_account_box</Symbol>
+                </Link>
+            </IfIsLoggedIn>
         </div>
     </div>)
 }
