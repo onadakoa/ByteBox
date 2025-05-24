1c8bc0e8c64868dcba735f0b20ebce3405ac13ce
onadakoa <onadakoa@gmail.com>
2025-05-24 14:48:14 +0200

    Modal in "client" pages

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -1,13 +1,16 @@
 import css from "./layout.module.css"
 import NavBar from "@/components/NavBar/NavBar"
+import {ModalProvider} from "@/components/Modal/ModalProvider";
 
 
 export default function Layout({children}: { children: React.ReactNode }) {
 
-    return (<>
-        <NavBar/>
-        <main className={css.container}>
-            {children}
-        </main>
-    </>)
+    return (
+        <ModalProvider>
+            <NavBar/>
+            <main className={css.container}>
+                {children}
+            </main>
+        </ModalProvider>
+    );
 }
