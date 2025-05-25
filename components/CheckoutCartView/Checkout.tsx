e95169b695fd2d7285408f9382911df0f9f8fc76
onadakoa <onadakoa@gmail.com>
2025-05-25 21:26:07 +0200

    Add PaymentGate modal integration on Pay button click

--- components/CheckoutCartView/Checkout.tsx
+++ components/CheckoutCartView/Checkout.tsx
@@ -4,6 +4,7 @@ import {Poppins, Roboto} from "next/font/google";
 import Button from "@/components/Button/Button";
 import {useCart} from "@/hooks/useCart";
 import {useModal} from "@/utils/ModalContext";
+import {PaymentGate} from "@/components/CheckoutCartView/PaymentGate";
 
 const poppins = Poppins({subsets: ["latin"], weight: ["100", "200", "400"]});
 const roboto = Roboto({subsets: ["latin"], weight: ["900", "700", "500"]});
@@ -20,11 +21,15 @@ export default function CheckOut() {
     const {info, isLoading, error} = useCart();
     if (isLoading || error) return;
 
+    const onPay = async () => {
+        showModal((<PaymentGate/>));
+    }
+
     return (<div className={css.container}>
         <div className={css.list}>
             <ListItem title={"items:"} value={`${info.count} (${info.total_quantity})`}/>
             <ListItem title={"To pay:"} value={info.total_price + " zł"}/>
         </div>
-        <Button>Pay</Button>
+        <Button onClick={onPay}>Pay</Button>
     </div>)
 }
